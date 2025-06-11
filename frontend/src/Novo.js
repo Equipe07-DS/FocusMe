60e2dfccfe0d99e7732d2288cc6fc58dd15bed22
alicemaca <maria.camorim@ufpe.br>
2025-06-10 21:38:01 -0300

    Adição do código js e do css da página de inputs para fazer um novo cronograma

---
+++ frontend/src/Novo.js
@@ -0,0 +1,23 @@
+import FazerFormulario from './componentes/FazerFormulario'
+
+import './Novo.css';
+
+function Novo(cronograma_output) {
+  return (
+
+    <div className="Quadro-b">
+      
+        <div className='Caixa-titulo'>
+          <h1 className='Tit'>Crie seu cronograma de estudos personalizado</h1>
+        </div>
+        <div className='Formulario'>
+          <FazerFormulario></FazerFormulario>
+        </div>
+        
+
+
+    </div>
+  );
+}
+
+export default Novo;
