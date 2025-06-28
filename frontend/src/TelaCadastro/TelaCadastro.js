179eaff8fb6e5e0f8a158a63d91d0df0caed2c88
adsonviana <adsonaviana@gmail.com>
2025-06-28 12:47:23 -0300

    Corrigindo maneira de estilização da tela de cadastro

--- frontend/src/TelaCadastro/TelaCadastro.js
+++ frontend/src/TelaCadastro/TelaCadastro.js
@@ -1,19 +1,22 @@
-import "./TelaCadastro.css";
 import FormsCadastro from "../componentes/FormsCadastro/FormsCadastro";
+import styles from "./TelaCadastro.module.css";
 
 
 export default function TelaCadastro(){
 
   return(
+    
     // Div geral da página
     <div className="w-screen h-screen flex flex-row bg-[#004E7E]">
 
       {/* Div geral da logo, fundo azul e mensagem*/}
       <div className="bg-[#004E7E] w-5/12 h-full flex flex-col justify-center items-center ">
-        <img src="/Logo-fundo-transparente.png" alt="Logo do FocusMe em fundo transparente" className="Logo-Transparente"/>
-        <h1 className="Texto-Logo text-white font-medium opacity-90 text-center mt-5">
+
+        <img src="/Logo-fundo-transparente.png" alt="Logo do FocusMe em fundo transparente" className={`${styles.LogoTransparente}`} />
+        <h1 className={`${styles.TextoLogo} text-white font-medium opacity-90 text-center mt-5 `} >
           Bem-vindo ao FocusMe, aqui você pode criar um cronograma semanal de estudos personalizável em instantes!
-          </h1>
+        </h1>
+
       </div>
 
 
@@ -22,11 +25,8 @@ export default function TelaCadastro(){
         
         <h1 className="flex justify-center text-[#004E7E] font-bold text-4xl">Olá, registre-se</h1>
         <h1 className="flex justify-center text-[#004E7E] font-bold text-4xl mb-7">para começar!</h1>
-        
-
-
+      
         <FormsCadastro/>
-
 
       </div>
 
--- frontend/src/componentes/FormsCadastro/FormsCadastro.js
+++ frontend/src/componentes/FormsCadastro/FormsCadastro.js
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import './FormsCadastro.css';
+import styles from './FormsCadastro.module.css';
 
 // Falta ajustar a integração ainda, apenas está com o exemplo da integração de outra tela só para não dar erro
 const FormsCadastro = () => {
@@ -42,40 +42,40 @@ const FormsCadastro = () => {
   return (
     <div>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="flex-col">
+      <form className={`${styles.FormCadastro} flex-col `} onSubmit={handleSubmit(onSubmit)}>
 
-        <div className="Inputs-Cadastro flex flex-col">
+        <div className="flex flex-col">
         
-          <label>Nome</label>
-          <input placeholder="Digite aqui"{...register('name')} />
-        
-        </div>
-
-        <div className="Inputs-Cadastro flex flex-col">
-        
-          <label>E-mail</label>
-          <input placeholder="Digite aqui"{...register('email')} />
+          <label className={`${styles.LabelCadastro}`} >Nome</label>
+          <input className={`${styles.InputsCadastro}`}  placeholder="Digite aqui"{...register('name')} />
         
         </div>
 
+        <div className=" flex flex-col">
+        
+          <label className={`${styles.LabelCadastro}`}>E-mail</label>
+          <input className={`${styles.InputsCadastro}`}  placeholder="Digite aqui"{...register('email')} />
+        
+        </div>
 
-        <div className="Inputs-Cadastro flex flex-col">
 
-          <label>Senha</label>
-          <input type="password" placeholder="Digite aqui"{...register('password')} />
+        <div className=" flex flex-col">
+
+          <label className={`${styles.LabelCadastro}`}>Senha</label>
+          <input className={`${styles.InputsCadastro}`}  type="password" placeholder="Digite aqui"{...register('password')} />
 
         </div>
 
-        <div className="Inputs-Cadastro flex flex-col mb-4">
+        <div className=" flex flex-col mb-4">
 
-          <label>Confirme sua senha</label>
-          <input type="password" placeholder="Digite aqui"{...register('password')} />
+          <label className={`${styles.LabelCadastro}`}>Confirme sua senha</label>
+          <input className={`${styles.InputsCadastro}`}  type="password" placeholder="Digite aqui"{...register('password')} />
 
         </div>
 
         <div className="flex flex-col gap-1 items-center">
 
-          <button className="botao" type="submit">Cadastrar</button>
+          <button className={`${styles.Botao}`} type="submit">Cadastrar</button>
 
           <button className="text-blue-500 text-sm hover:text-blue-800 hover:underline" onClick={handleVerCronograma}>Já possui uma conta? Faça login agora</button>
         
