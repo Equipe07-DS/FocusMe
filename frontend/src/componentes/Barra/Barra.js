e5fed8b14035c766af9583f1f71e558d0965474b
alicemaca <maria.camorim@ufpe.br>
2025-07-08 13:17:53 -0300

    estrutura

--- frontend/src/componentes/Barra/Barra.js
+++ frontend/src/componentes/Barra/Barra.js
@@ -3,7 +3,26 @@ import styles from './Barra.module.css';
 
 
 const Barra = () => {
-  
+  return (
+        <nav>
+            <div>
+                <button>
+                    <img src="/Logo-fundo-transparente.png" alt="Logo do FocusMe em fundo transparente"/>
+                </button>
+                
+            </div>
+            <ul>
+                <li>
+                    <a href="/inicio">Inicio</a>
+                </li>
+                <li>
+                    <a href="/novocronograma">Cronograma</a>
+                </li>
+                <li><a href="">Chat</a></li>
+                <li><a href="">Perfil</a></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Barra;
