ebe99113299dae7239322e6c4e91ffef0ff00ed2
alicemaca <maria.camorim@ufpe.br>
2025-07-22 20:50:52 -0300

    estrutura da tela

--- frontend/src/TelaInicial/TelaInicial.js
+++ frontend/src/TelaInicial/TelaInicial.js
@@ -1,1 +1,18 @@
+import Barra from "../componentes/Barra/Barra";
+import styles from "./TelaInicial.module.css";
 
+
+export default function TelaInicial(){
+
+    return(
+
+        <div className="bg-[#5A9AC4] w-screen h-screen">
+            <Barra/>
+            <div className="flex flex-wrap justify-center items-center">
+                <div className={`${styles.Quadro}`}></div>
+            </div>
+            
+        </div>
+    )
+
+}
