19270052a3b556fd6796b6985644fbbd47ca561c
adsonviana <adsonaviana@gmail.com>
2025-07-23 13:09:46 -0300

    ajustando componente formulário de login

--- frontend/src/componentes/FormsLogin/FormsLogin.js
+++ frontend/src/componentes/FormsLogin/FormsLogin.js
@@ -46,53 +46,74 @@ const FormsLogin = () => {
   const camposPreenchidos = email && senha;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)} className={`${styles.FormLogin}`}>
 
-        <div className={`${styles.CaixaInputsLogin} w-full flex flex-col mb-2`}>
-          <label className={`${styles.LabelLogin}`}>E-mail</label>
-          <input
-            className={`${styles.InputsLogin}`}
-            placeholder="Digite aqui"
-            {...register('email', { required: true })}
-          />
+   
+    <div className="w-screen h-screen flex flex-row bg-[#004E7E]">
+
+      
+      <div className={`bg-[#004E7E] w-5/12 h-full flex flex-col justify-center items-center`}>
+        <img src="/Logo-fundo-transparente.png" alt="Logo do FocusMe em fundo transparente" className={`${styles.LogoTransparente}`}/>
+        <h1 className={`${styles.TextoLogo} text-white font-medium opacity-90 text-center mt-5`}>
+          Bem-vindo ao FocusMe, aqui você pode criar um cronograma semanal de estudos personalizável em instantes!
+          </h1>
+      </div>
+
+
+      <div className=" bg-white w-7/12 h-full rounded-l-3xl px-20 py-20 flex-col">
+        
+        <h1 className="flex justify-center text-[#004E7E] font-bold text-4xl mb-5 mt-16">Bem-vindo de volta!</h1>
+        
+
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
+
+            <div className="flex flex-col">
+              <label className={`${styles.LabelLogin}`}>E-mail</label>
+              <input
+                className={`${styles.InputsLogin}`}
+                placeholder="E-mail"
+                {...register('email', { required: true })}
+              />
+            </div>
+
+            <div className="flex flex-col mb-4">
+              <label className={`${styles.LabelLogin}`}>Senha</label>
+              <input
+                className={`${styles.InputsLogin}`}
+                type="senha"
+                placeholder="Senha"
+                {...register('senha', { required: true })}
+              />
+            </div>
+
+            <div className="flex flex-col gap-1 items-center">
+              <button
+                className={`${styles.Botao} hover:opacity-70`}
+                type="submit"
+                disabled={!camposPreenchidos}
+              >
+                Entrar
+              </button>
+
+              <button
+                type="button"
+                className="text-blue-500 text-sm hover:text-blue-800 hover:underline"
+                onClick={handleFazerCadastro}
+              >
+                Primeira vez por aqui? Faça seu cadastro
+              </button>
+
+              {loginStatus && (
+                <p style={{ marginTop: '10px', color: loginStatus === 'sucesso' ? 'green' : 'red' }}>
+                  {loginStatus === 'sucesso' ? 'Login bem-sucedido!' : loginStatus}
+                </p>
+              )}
+            </div>
+          </form>
         </div>
 
-        <div className={`${styles.CaixaInputsLogin} w-full flex flex-col mb-14`}>
-          <label className={`${styles.LabelLogin}`}>Senha</label>
-          <input
-            className={`${styles.InputsLogin}`}
-            type="senha"
-            placeholder="Digite aqui"
-            {...register('senha', { required: true })}
-          />
-        </div>
+      </div>
 
-        <div className="flex flex-col gap-1 items-center">
-          <button
-            className={`${styles.Botao}`}
-            type="submit"
-            disabled={!camposPreenchidos}
-            style={{ opacity: camposPreenchidos ? 1 : 0.5 }}
-          >
-            Entrar
-          </button>
-
-          <button
-            type="button"
-            className="text-blue-500 text-sm hover:text-blue-800 hover:underline"
-            onClick={handleFazerCadastro}
-          >
-            Primeira vez por aqui? Faça seu cadastro
-          </button>
-
-          {loginStatus && (
-            <p style={{ marginTop: '10px', color: loginStatus === 'sucesso' ? 'green' : 'red' }}>
-              {loginStatus === 'sucesso' ? 'Login bem-sucedido!' : loginStatus}
-            </p>
-          )}
-        </div>
-      </form>
     </div>
   );
 };
