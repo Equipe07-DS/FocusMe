7a8a64b338d5f403a3b2c58b0e49457265ba857c
Maria Alice <maria.camorim@ufpe.br>
2025-07-23 01:03:13 -0300

    título

--- frontend/src/TelaInicial/TelaInicial.js
+++ frontend/src/TelaInicial/TelaInicial.js
@@ -9,7 +9,9 @@ export default function TelaInicial(){
         <div className="bg-[#5A9AC4] w-screen h-screen">
             <Barra/>
             <div className="flex flex-wrap justify-center items-center">
-                <div className={`${styles.Quadro}`}></div>
+                <div className={`${styles.Quadro}`}>
+                    <h1 className={`${styles.titulo}`}>Bem vindo(a)</h1>
+                </div>
             </div>
             
         </div>
