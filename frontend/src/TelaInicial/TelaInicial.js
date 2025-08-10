733d429e5deef7b5a35324379629ec893b32f457
alicemaca <maria.camorim@ufpe.br>
2025-08-10 16:18:58 -0300

    alteração do texto TelaInicial.js

--- frontend/src/TelaInicial/TelaInicial.js
+++ frontend/src/TelaInicial/TelaInicial.js
@@ -11,8 +11,12 @@ export default function TelaInicial(){
             <div className="flex flex-wrap justify-center items-center">
                 <div className={`${styles.conteudo}`}>
                     <div className={`${styles.Quadro}`}>
-                        <h1 className={`${styles.titulo}`}>Bem vindo(a)</h1>
-                        <p className={`${styles.texto}`}>  Você pode consultar seu cronograma semanal na aba cronograma, conversar com nosso assistente virtual, Fabio, para alterar seu cronograma na aba de chat e ver suas informações de perfil na aba de perfil. Boa joranda de estudos! </p>
+                        <h1 className={`${styles.titulo}`}>Bem vindo(a) ao FocusMe!</h1>
+                            <p className={`${styles.texto}`}>  Nosso objetivo é te ajudar a organizar seus horários de estudo de forma eficiente e flexível.</p>
+                            <p className={`${styles.texto}`}>  Aqui você pode: </p>
+                            <p className={`${styles.texto}`}> .Consultar seu cronograma semanal na aba cronograma. </p>
+                            <p className={`${styles.texto}`}> .Pedir ao nosso assistente virtual para alterar seu cronograma na aba de chat. </p>
+                            <p className={`${styles.texto}`}> Boa jornada de estudos!</p>
                         <p className={`${styles.texto}`}>Atividades do dia:</p>
                         <div className={`${styles.Quadro_dia}`}>
                             <p className={`${styles.texto_dia}`}>==Cronograma do dia==</p>
