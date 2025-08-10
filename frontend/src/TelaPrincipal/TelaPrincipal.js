6aa854b4cabd5ff252a5e101b8a62cf48ebccdb9
alicemaca <maria.camorim@ufpe.br>
2025-08-10 14:13:08 -0300

    top bar da tela principal

--- frontend/src/TelaPrincipal/TelaPrincipal.js
+++ frontend/src/TelaPrincipal/TelaPrincipal.js
@@ -1,1 +1,25 @@
+import { Link, useNavigate } from "react-router-dom";
+import styles from './TelaPrincipal.module.css';
 
+export default function TelaLoginn(){
+
+  return(
+    <div className="fundo">
+        <div className="">
+            <nav className={`${styles.Barra}`}>
+                <div>
+                    <button className={`${styles.botao_logo}`} >
+                        <img src="/Logo-fundo-transparente.png" alt="Logo do FocusMe em fundo transparente" className={`${styles.logo}`}/>
+                    </button>
+                </div>
+                <ul className="flex flex-row gap-10 pointer">
+                    <li className={`${styles.caixa_botoes}`}>
+                        <Link to="/login" className={`${styles.texto_botoes}`}>Login</Link>
+                    </li>
+                    <li className={`${styles.caixa_botao_cadastro}`}>
+                        <Link to="/cadastro" className={`${styles.texto_botao_cadastro}`}>Cadastre-se</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+  </div>
