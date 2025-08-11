243fa45a8b56e3a83139d44d9f39c46cfbf1845c
alicemaca <maria.camorim@ufpe.br>
2025-08-11 01:15:13 -0300

    Update database.py

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,50 +1,22 @@
-import os
-import time
-import schedule
-import dropbox
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-DROPBOX_TOKEN = os.getenv("DROPBOX_T")  
-DROPBOX_DB_PATH = "/focusdatabase.db"  
-LOCAL_DB_PATH = "./focusdatabase.db"   
-
-
-def baixar_banco():
-    """Baixa o banco de dados do Dropbox para o local"""
-    dbx = dropbox.Dropbox(DROPBOX_TOKEN)
-    try:
-        print("📥 Baixando banco do Dropbox...")
-        metadata, res = dbx.files_download(DROPBOX_DB_PATH)
-        with open(LOCAL_DB_PATH, "wb") as f:
-            f.write(res.content)
-        print("✅ Banco atualizado localmente.")
-    except dropbox.exceptions.ApiError:
-        print("⚠️ Banco não encontrado no Dropbox, será criado localmente.")
-
-def subir_banco():
-    """Envia o banco local para o Dropbox"""
-    dbx = dropbox.Dropbox(DROPBOX_TOKEN)
-    if os.path.exists(LOCAL_DB_PATH):
-        with open(LOCAL_DB_PATH, "rb") as f:
-            print("📤 Enviando banco para o Dropbox...")
-            dbx.files_upload(f.read(), DROPBOX_DB_PATH, mode=dropbox.files.WriteMode("overwrite"))
-            print("✅ Banco enviado com sucesso.")
-    else:
-        print("⚠️ Banco local não encontrado.")
-
-
-DATABASE_URL = f"sqlite:///{LOCAL_DB_PATH}"
+DATABASE_URL = "sqlite:///./focusdatabase.db"  
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+try:
+    Base.metadata.create_all(bind=engine)
+    print("Tabelas do banco de dados criadas ou já existentes.")
+except Exception as e:
+    print(f"Erro ao criar as tabelas do banco de dados: {e}")
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_session():
     db = SessionLocal()
